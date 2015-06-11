aed0670b7f7e7216d920585b0be6d7386541b8c9
Vijayalakshmi22 <vijayalakshmi.gottipati2@gmail.com>
2015-06-11 17:30:33 +0530

    uploaded

---
+++ Expressions-20150611.py
@@ -0,0 +1,54 @@
+xx=2
+xx=xx+2
+print xx
+
+yy=440*12
+print yy
+
+zz=yy/1000
+print zz
+
+jj=23
+kk=jj%5
+print kk
+
+print 4**3
+
+x=1+2**3/4*5
+print x
+
+print 10/2        #Integer Division
+print 99/100      #Integer division,it will truncate the decimals
+print 99.0/100.0  #Floating point Division
+
+print 99/100.0    #Floating point Division
+print 99.0/100    #Floating point Division
+
+ddd=1+4
+print ddd
+
+eee='hello'+'there'
+print eee
+
+type(eee)
+type(1)
+
+print float(99)/100
+i=42
+type(i)
+f=float(i)
+print f
+type(f)
+
+print 1+2*float(3)/4-5
+
+nam=raw_input("Who are you")
+print 'welcome',nam
+
+inp=raw_input("Europe floor?")
+usf=int(inp)+1
+
+hours=raw_input("enter the no.of hours")
+rate=raw_input("enter the rate")
+pay=float(hours)*float(rate)
+print pay
