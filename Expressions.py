ed24cba11e6c8653f0ca7401d49d0ad54d8a1932
Vijayalakshmi22 <vijayalakshmi.gottipati2@gmail.com>
2015-06-11 16:12:06 +0530

    uploaded

---
+++ Expressions.py
@@ -0,0 +1,25 @@
+xx=2
+xx=xx+2
+print xx
+
+yy=440*12
+print yy
+
+zz=yy/1000
+print zz
+
+jj=23
+kk=jj%5
+print kk
+
+print 4**3
+
+x=1+2**3/4*5
+print x
+
+print 10/2        #Integer Division
+print 99/100      #Integer division,it will truncate the decimals
+print 99.0/100.0  #Floating point Division
+
+print 99/100.0    #Floating point Division
+print 99.0/100    #Floating point Division
